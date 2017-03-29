37f643d968328cdb342c8f115fb46efba8052489
moses nandwa <nandwa.moses@gmail.com>
2017-03-29 12:58:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,77 +1,35 @@
 <?php
-/**
- * Webhook for Time Bot- Facebook Messenger Bot
- * User: adnan
- * Date: 24/04/16
- * Time: 3:26 PM
- */
-$access_token = "EAAH5qCyWCdcBAP0ddTZBNbVRdmqd43TZCnBJGFEwRZAmO76hlrXfWmVzBXO5xEsochEnlrQ88Tkrwm2B63KzXctLxXQ8RU6KKM9sWEFsGZAaBzmmMUoqVjfir1n5ufXgW8btvZAL41bNJ5S0IceHKUCioOLTqCLZCZCOOMlNz5fRAZDZD";
-$verify_token = "fb_time_bot";
-$hub_verify_token = null;
+// parameters
+$hubVerifyToken = 'TOKEN123456abcd';
+$accessToken = "EAAbjcuvajRYBAKc0O857d7sBYqjFc18uZBlCaBP2LK3YGZCuueQAG6DHC66AMQi534qC3ZADgZBWLTZCGOMISZAYRBu2cQ6g6wcB7S6WYTESbWSGoRkY4Wts0N0f4z19nx28jWwFaYWBMZBeXKgQUrr249BgVtIao4RsP4AE1a5EQZDZD";
 
-if(isset($_REQUEST['hub_challenge'])) {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
+// check token at setup
+if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+  echo $_REQUEST['hub_challenge'];
+  exit;
 }
 
-
-if ($hub_verify_token === $verify_token) {
-    echo $challenge;
-}
-
+// handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
 
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 
-$message_to_reply = '';
 
-/**
- * Some Basic rules to validate incoming messages
- */
-if(preg_match('[time|current time|now]', strtolower($message))) {
-
-    // Make request to Time API
-    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
-    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
-    if($result != '') {
-        $message_to_reply = $result;
-    }
-} else {
-    $message_to_reply = 'Huh! what do you mean?';
+$answer = "I don't understand. Ask me 'hi'.";
+if($messageText == "hi") {
+    $answer = "Hello";
 }
 
-//API Url
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
-
-
-//Initiate cURL.
-$ch = curl_init($url);
-
-//The JSON data.
-$jsonData = '{
-    "recipient":{
-        "id":"'.$sender.'"
-    },
-    "message":{
-        "text":"'.$message_to_reply.'"
-    }
-}';
-
-//Encode the array into JSON.
-$jsonDataEncoded = $jsonData;
-
-//Tell cURL that we want to send a POST request.
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_exec($ch);
+curl_close($ch);
 
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-
-//Execute the request
-if(!empty($input['entry'][0]['messaging'][0]['message'])){
-    $result = curl_exec($ch);
-}
+//based on http://stackoverflow.com/questions/36803518
